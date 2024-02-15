523dc1242dccbf612d9f9819ca24cdef4dd354bd
Rafael Lopez <lopezrafael.b81@gmail.com>
2024-02-15 16:59:04 -0500

    Docs creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { CounterModule } from './counter/counter.module';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/heroes.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DbzModule } from './dbz/dbz.module';
 
 
 @NgModule({
